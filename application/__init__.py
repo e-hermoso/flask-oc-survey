3debea4f148746e3d0dabe43f9a37ca708702ca7
e-hermoso <erichermoso5@gmail.com>
2021-09-25 14:11:14 -0700

    edit notes

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,6 @@
 '''
 The __init__.py file is the default file for initialization; similar to index.js file in React or Vue.
-Here we move the app object into this file. The application needs to know where it starts by looking into
+Here we define the app object. The application needs to know where it starts by looking into
 the main.py file thats because we defined our enviounrment to look into the main.py file. In our main.py file
 we import and load the app object.
 '''
