27e2c913aa45ffa7ef31457668523172ec51cd98
e-hermoso <erichermoso5@gmail.com>
2021-09-25 13:59:16 -0700

    Added notes

--- application/__init__.py
+++ application/__init__.py
@@ -1,3 +1,9 @@
+'''
+The __init__.py file is the default file for initialization; similar to index.js file in React or Vue.
+Here we move the app object into this file. The application needs to know where it starts by looking into
+the main.py file thats because we defined our enviounrment to look into the main.py file. In our main.py file
+we import and load the app object.
+'''
 from flask import Flask
 # Special variable (__name__) to identify the current application or module that is being rendered or passed to flask
 app = Flask(__name__)
